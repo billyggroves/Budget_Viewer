8f27479fb0b7bae023209f82d964ef3f16062d69
billyggroves <billyggroves@gmail.com>
2025-10-11 18:31:38 -0400

    fix data load, exclude CC payments and separate debit and credit transactions

--- data_loader.py
+++ data_loader.py
@@ -1,47 +1,79 @@
 import csv
 import os
-from transaction import Transaction, Transaction_Type
+from transaction import Transaction
 
 """
     TODO
-        - Create categorys, with rules
-        - Fix bug with Negative transactions in Checking vs CC payments
+        - Create categorys, with rules and potentially create more branches in transactions{}
 """
 
 
 def load():
-    transactions = []
+    transactions = {}
+    transactions['credit'] = []
+    transactions['debit'] = []
     directory = "./data"
     for file in os.listdir(directory):
         with open(directory + '/' + file, mode='r') as file:
             reader = csv.reader(file)
-            description = 0
-            debit = 0
-            credit = 0
+            print(f"loading.... {file.name}")
+            date = None
+            t_type = None
+            description = None
+            amount = None
+            debit = None
+            credit = None
             for i, row in enumerate(reader):
                 if i == 0:
+                    if 'Date' in row:
+                        date = row.index('Date')
                     if 'Transaction' in row:
-                        transaction_type = row.index('Transaction')
+                        t_type = row.index('Transaction')
                     if 'Description' in row:
                         description = row.index('Description')
-                    elif 'Name' in row:
+                    if 'Name' in row:
                         description = row.index('Name')
                     if 'Amount' in row:
-                        debit = row.index('Amount')
+                        amount = row.index('Amount')
                     if 'Debit' in row:
                         debit = row.index('Debit')
                     if 'Credit' in row:
                         credit = row.index('Credit')
-                    # print(f"indexes: {description}, {debit}, {credit}")
+                    print(f"indexes: date: {date}, t_type: {t_type}, desc: {description}, amount: {amount}, debit: {debit}, credit: {credit}")
                     continue
-                if row[debit] == '':
-                    transaction = Transaction(row[description],Transaction_Type.CREDIT,abs(float(row[credit])))
-                elif float(row[debit]) < 0 or (transaction_type and transaction_type == "CREDIT"):
-                    transaction = Transaction(row[description],Transaction_Type.CREDIT,abs(float(row[debit])))
+                if date is not None and description is not None:
+                    # TODO Find better way to achieve this, as it could cause issues
+                    # potential fixes, if (name of CC company in ...)
+                    if "payment" in row[description].lower():
+                        continue
+                    if (debit is None 
+                    and credit is None 
+                    and amount is not None):
+                        if t_type is not None:
+                            if row[t_type] == "DEBIT":
+                                transactions['debit'].append(Transaction(row[date],row[description],float(row[amount])))
+                            elif row[t_type] == "CREDIT":
+                                transactions['credit'].append(Transaction(row[date],row[description],float(row[amount])))
+                        else:
+                            if float(row[amount]) > 0.0:
+                                transactions['debit'].append(Transaction(row[date],row[description],float(row[amount])))
+                            else:
+                                transactions['credit'].append(Transaction(row[date],row[description],float(row[amount])))
+                    elif debit is not None and credit is not None:
+                        if row[credit] == '':
+                            transactions['debit'].append(Transaction(row[date],row[description],float(row[debit])))
+                        else:
+                            transactions['credit'].append(Transaction(row[date],row[description],float(row[credit])))
+                    else:
+                        print(f"something is wrong with this transaction: {row}")
                 else:
-                    transaction = Transaction(row[description],Transaction_Type.DEBIT,abs(float(row[debit])))
-                transactions.append(transaction)
-    for tran in transactions:
+                    print(f"something is wrong with this transaction or CSV: {row}")
+            print(f"{len(transactions['credit']) + len(transactions['debit'])} transactions loaded")
+    for tran in transactions['credit']:
+        print("Credit")
+        print(tran)
+    for tran in transactions['debit']:
+        print("Debit")
         print(tran)
     return transactions
     
--- transaction.py
+++ transaction.py
@@ -1,15 +1,8 @@
-from enum import Enum
-
-class Transaction_Type(Enum):
-    DEBIT = "DEBIT"
-    CREDIT = "CREDIT"
-
-
 class Transaction:
-    def  __init__(self, description, transaction_type, amount):
+    def  __init__(self, date, description, amount):
+        self.date = date
         self.description = description
-        self.transaction_type = transaction_type
         self.amount = abs(amount)
 
     def __repr__(self):
-        return f"Transaction: {self.description}, {self.transaction_type}, ${self.amount}"
+        return f"Transaction: {self.date}, {self.description}, ${self.amount}"
