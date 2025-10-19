23ff700360def63eca34516566ceb2d9336a495d
billyggroves <billyggroves@gmail.com>
2025-10-19 00:10:32 -0400

    Add TODOs

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from category_loader import load_categories
 from diagram import build_diagram
 
+"""
+TODO:
+    Add testing
+    Edge cases
+        Negative Profit
+        Rethink handling of CC payments, count or ignore?
+    Add more transaction label filtering
+    Change Profit to Savings
+    General cleanup and refactoring
+    Sankey frontend, make it pretty :D
+    Remove the blank node?
+    Add lables to links?
+"""
+
 def main():
     categories = load_categories()
     build_diagram(categories)
